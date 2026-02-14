9cec58fb0a8bf04b816bb1f87410536a17eadd75
Mark Malstrom <mark@malstrom.me>
2026-02-14 12:44:23 -0600

    Consolidate show & edit endpoints

--- src/routes/contacts/index.ts
+++ src/routes/contacts/index.ts
@@ -1,6 +1,6 @@
 export * from "./contacts.create.ts";
 export * from "./contacts.destroy.ts";
-export * from "./contacts.edit.tsx";
 export * from "./contacts.favorite.ts";
 export * from "./contacts.show.tsx";
+export { show as edit } from "./contacts.show.tsx";
 export * from "./contacts.update.ts";
