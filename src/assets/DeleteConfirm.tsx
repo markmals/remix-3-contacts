bc09d01362eb6da15e5b5177aa6d29ac410fd3b1
Mark Malstrom <mark@malstrom.me>
2026-02-16 16:51:56 -0600

    Simplify delete button submit handler logic

--- src/assets/DeleteConfirm.tsx
+++ src/assets/DeleteConfirm.tsx
@@ -1,11 +1,9 @@
-import { clientEntry, type Handle } from "remix/component";
+import { clientEntry } from "remix/component";
 import { routes } from "~/routes.ts";
 
 export const DeleteConfirm = clientEntry(
     "/assets/DeleteConfirm.js#DeleteConfirm",
-    function DeleteConfirm(handle: Handle) {
-        let submitting = false;
-
+    function DeleteConfirm() {
         return (props: { contactId: number }) => (
             <form
                 action={routes.contacts.destroy.href({ id: props.contactId })}
@@ -14,30 +12,12 @@ export const DeleteConfirm = clientEntry(
                     async submit(event) {
                         if (!confirm("Please confirm you want to delete this record.")) {
                             event.preventDefault();
-                            return;
                         }
-
-                        event.preventDefault();
-
-                        submitting = true;
-                        await handle.update();
-
-                        await fetch(event.currentTarget.action, {
-                            method: event.currentTarget.method,
-                            body: new FormData(event.currentTarget),
-                        });
-
-                        await navigation.navigate(routes.home.href(), { history: "push" }).finished;
-
-                        submitting = false;
-                        await handle.update();
                     },
                 }}
             >
                 <input name="_method" type="hidden" value="DELETE" />
-                <button disabled={submitting} type="submit">
-                    Delete
-                </button>
+                <button type="submit">Delete</button>
             </form>
         );
     },
