7af68c775a403381715b193902740836f560bc93
Mark Malstrom <mark@malstrom.me>
2026-03-23 09:57:40 -0500

    Update render functions

--- src/lib/render.tsx
+++ src/lib/render.tsx
@@ -1,24 +1,24 @@
 import { getContext } from "remix/async-context-middleware";
-import type { RemixNode } from "remix/component/jsx-runtime";
+import type { RemixNode } from "remix/component";
 import { renderToStream } from "remix/component/server";
-import { createHtmlResponse } from "remix/response/html";
+import { createHtmlResponse as html } from "remix/response/html";
 import { router } from "~/router.tsx";
 
 export const render = {
     // Using createHtmlResponse inserts DOCTYPE at the beginning of the document
     // For frame partials, we don't want to include DOCTYPE
-    frame(node: JSX.Element): Response {
+    frame(node: RemixNode): Response {
         return new Response(renderToStream(node), {
             headers: { "Content-Type": "text/html; charset=utf-8" },
         });
     },
     document(node: RemixNode): Response {
-        const ctx = getContext();
-        return createHtmlResponse(
+        const context = getContext();
+        return html(
             renderToStream(node, {
-                frameSrc: ctx.url,
-                async resolveFrame(src, _target, context) {
-                    const url = new URL(src, context?.currentFrameSrc ?? ctx.url);
+                frameSrc: context.url,
+                async resolveFrame(src, _target, ctx) {
+                    const url = new URL(src, ctx?.currentFrameSrc ?? context.url);
                     const response = await router.fetch(
                         new Request(url, { headers: { accept: "text/html" } }),
                     );
