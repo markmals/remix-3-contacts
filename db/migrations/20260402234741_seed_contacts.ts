d06cc93eac7a74ad0333f96189252206431c213b
Mark Malstrom <mark@malstrom.me>
2026-05-01 23:28:52 -0500

    Inline contacts table definitions into migrations so they don't break when the canonical app definition is changed

--- db/migrations/20260402234741_seed_contacts.ts
+++ db/migrations/20260402234741_seed_contacts.ts
@@ -1,5 +1,19 @@
-import { Contacts } from "#/data/contacts.ts";
+import { column as c, ColumnBuilder, table } from "remix/data-table";
 import { createMigration } from "remix/data-table/migrations";
+
+export let Contacts = table({
+    name: "contacts",
+    columns: {
+        id: c.integer().primaryKey(),
+        first: c.text().notNull(),
+        last: c.text().notNull(),
+        avatar: c.text(),
+        bsky: c.text().notNull(),
+        notes: c.text().notNull(),
+        favorite: c.boolean().default(false),
+        createdAt: c.timestamp().defaultNow() as ColumnBuilder<string>,
+    },
+});
 
 const SEED_CONTACTS = [
     {
